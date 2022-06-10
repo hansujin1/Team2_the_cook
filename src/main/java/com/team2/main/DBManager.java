9f057424e205e803172a4835a4718f1a65eb0e48
sujin <gks365199@gmail.com>
2022-06-10 09:33:34 +0900

    First commit

---
+++ src/main/java/com/team2/main/DBManager.java
@@ -0,0 +1,39 @@
+package com.team2.main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+//DB관련 작업을 할때 매번 연결코드를 쓴 이후 작업 해둠
+
+//그거 AOP 하자
+public class DBManager {
+
+	//db작업시엔 어쨋든 연결 해야됨
+	public static Connection connect() throws SQLException {
+		
+		String url ="jdbc:oracle:thin:@localhost:1521:xe";
+		return DriverManager.getConnection(url,"c##sj","sj");
+	}
+	
+	//닫을게 많은데 한번에 닫기
+	public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+		
+		try {
+			if (rs != null) {				
+				rs.close();
+			}
+			pstmt.close();
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
