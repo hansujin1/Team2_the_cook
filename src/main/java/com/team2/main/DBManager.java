b92bd236600d56a1664ac126e9dbedd69abdc7ee
WonbongKIM <jinyuanfeng5@gmail.com>
2022-06-16 12:47:50 +0900

    db수정

--- src/main/java/com/team2/main/DBManager.java
+++ src/main/java/com/team2/main/DBManager.java
@@ -13,29 +13,27 @@ import javax.naming.spi.DirStateFactory.Result;
 //그거 AOP 하자
 public class DBManager {
 
-	//db작업시엔 어쨋든 연결 해야됨
-	public static Connection connect() throws SQLException {
-		
-		String url ="jdbc:oracle:thin:@localhost:1521:xe";
+    //db작업시엔 어쨋든 연결 해야됨
+    public static Connection connect() throws SQLException {
 
-		return DriverManager.getConnection(url,"leeyoky","leeyoky");
+        String url ="jdbc:oracle:thin:@localhost:1521:xe";
+        return DriverManager.getConnection(url,"kwb","kwb");
+    }
 
-	}
-	
-	//닫을게 많은데 한번에 닫기
-	public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
-		
-		try {
-			if (rs != null) {				
-				rs.close();
-			}
-			pstmt.close();
-			con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-	}
+    //닫을게 많은데 한번에 닫기
+    public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            pstmt.close();
+            con.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+    }
 }
